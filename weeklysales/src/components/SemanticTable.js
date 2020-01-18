ab7bdaa8e01424600090f4e418ac55ec3510ec5d
JarredStanford <stanj89@gmail.com>
2020-01-17 21:21:38 -0500

    fixed table year order

--- weeklysales/src/components/SemanticTable.js
+++ weeklysales/src/components/SemanticTable.js
@@ -54,9 +54,9 @@ const SemanticTable = () => {
             <Form>
                 <Form.Group inline>
                     <Form.Field onChange={(e) => setYear(Number(e.target.value))} control='select' name='year' label='Year'>
-                        <option value={2017}>2017</option>
-                        <option value={2018}>2018</option>
                         <option value={2019}>2019</option>
+                        <option value={2018}>2018</option>
+                        <option value={2017}>2017</option>
                     </Form.Field>
                 </Form.Group>
             </Form>
